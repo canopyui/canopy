fa0e25aff757cabb03e00e75a6a816fe1144a9d3
Harlan T Wood <harlan@thegoldensun.com>
2013-02-21 17:57:22 -0800

    Add Reingold–Tilford Tree D3 layout
    See also http://bl.ocks.org/mbostock/4063550

--- app.d3.coffee
+++ app.d3.coffee
@@ -35,3 +35,47 @@ class root.App.D3
         .attr("text-anchor", "middle")
         .attr("dy", ".3em")
         .text (d) -> if d.name.length <= d.r/3 then d.name else ""
+
+
+  renderRadialTree: (data) ->
+    diameter = 960
+    tree = d3.layout.tree()
+      .size([360, diameter / 2 - 120])
+      .separation((a, b) -> ((if a.parent is b.parent then 1 else 2)) / a.depth)
+
+    diagonal = d3.svg.diagonal.radial()
+      .projection((d) -> [d.y, d.x / 180 * Math.PI])
+
+    svg = d3.select("body").append("svg")
+        .attr("width", diameter)
+        .attr("height", diameter - 150)
+      .append("g")
+        .attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")")
+
+    nodes = tree.nodes(data)
+    links = tree.links(nodes)
+    link = svg.selectAll(".link")
+        .data(links)
+      .enter()
+        .append("path")
+        .attr("class", "link")
+        .attr("d", diagonal)
+        
+    node = svg.selectAll(".node")
+        .data(nodes)
+      .enter()
+        .append("g")
+        .attr("class", "node")
+        .attr("transform", (d) -> "rotate(" + (d.x - 90) + ")translate(" + d.y + ")")
+        
+    node.append("circle")
+        .attr "r", 4.5
+        
+    node.append("text")
+        .attr("dy", ".31em")
+        .attr("text-anchor", (d) -> (if d.x < 180 then "start" else "end"))
+        .attr("transform", (d) -> (if d.x < 180 then "translate(8)" else "rotate(180)translate(-8)"))
+        .text (d) -> d.name
+
+    d3.select(self.frameElement).style "height", diameter - 150 + "px"
+  
--- app.d3.js
+++ app.d3.js
@@ -46,6 +46,41 @@
       });
     };
 
+    D3.prototype.renderRadialTree = function(data) {
+      var diagonal, diameter, link, links, node, nodes, svg, tree;
+      diameter = 960;
+      tree = d3.layout.tree().size([360, diameter / 2 - 120]).separation(function(a, b) {
+        return (a.parent === b.parent ? 1 : 2) / a.depth;
+      });
+      diagonal = d3.svg.diagonal.radial().projection(function(d) {
+        return [d.y, d.x / 180 * Math.PI];
+      });
+      svg = d3.select("body").append("svg").attr("width", diameter).attr("height", diameter - 150).append("g").attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
+      nodes = tree.nodes(data);
+      links = tree.links(nodes);
+      link = svg.selectAll(".link").data(links).enter().append("path").attr("class", "link").attr("d", diagonal);
+      node = svg.selectAll(".node").data(nodes).enter().append("g").attr("class", "node").attr("transform", function(d) {
+        return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")";
+      });
+      node.append("circle").attr("r", 4.5);
+      node.append("text").attr("dy", ".31em").attr("text-anchor", function(d) {
+        if (d.x < 180) {
+          return "start";
+        } else {
+          return "end";
+        }
+      }).attr("transform", function(d) {
+        if (d.x < 180) {
+          return "translate(8)";
+        } else {
+          return "rotate(180)translate(-8)";
+        }
+      }).text(function(d) {
+        return d.name;
+      });
+      return d3.select(self.frameElement).style("height", diameter - 150 + "px");
+    };
+
     return D3;
 
   })();
